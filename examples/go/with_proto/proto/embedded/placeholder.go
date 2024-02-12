f72cb14fbdf54af7b956d991a9ceb3c1da7f3559
Borja Lorente <blorente@apple.com>
2024-02-12 10:45:08 +0000

    Reproduce proto within proto

---
+++ examples/go/with_proto/proto/embedded/placeholder.go
@@ -0,0 +1,6 @@
+// Package embedded.
+// This file is left intentionally empty, as a way to reproduce this issue:
+//   - https://github.com/bazelbuild/rules_go/issues/2262
+//
+// It exists to be a container for the go files generated from the protobuf in this package.
+package embedded
--- examples/go/with_proto/proto/translators.go
+++ examples/go/with_proto/proto/translators.go
@@ -1,10 +1,15 @@
 package proto
 
 import (
+	"github.com/bazelbuild/intellij/examples/go/with_proto/proto/embedded"
 	"time"
 )
 
 // Time translation functions.
+
+func CreateMessage() embedded.Message {
+	return embedded.Message{}
+}
 
 func ToProtoTime(t time.Time) Time {
 	return Time{Value: t.Format(time.RFC3339Nano)}
